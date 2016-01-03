93b2efcc3c52ef3e2e63b29e122281183bd10b30
Zlobrynya <neket95@inbox.ru>
2016-01-03 18:02:41 +0300

    class Mang

--- app/src/main/java/com/example/nikita/progectmangaread/MainActivity.java
+++ app/src/main/java/com/example/nikita/progectmangaread/MainActivity.java
@@ -16,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void Click(View view) {
         Intent newInten = new Intent(MainActivity.this,temple_pase.class);
-        newInten.putExtra("URL","http://readmanga.me");
+        classMang readManga = new classMang("http://readmanga.me","[class=img] img[src]","[class=img] a","[class=tile col-sm-6]");
+        readManga.setWhere("/list", "?type=&sortType=rate&offset=", "&max=70", 0);
+        newInten.putExtra("URL", "http://readmanga.me");
         newInten.putExtra("Where","/list?type=&sortType=rate&offset=0&max=70");
         newInten.putExtra("Cell","[class=tile col-sm-6]");
         newInten.putExtra("nameURL", "[class=img] a");
@@ -36,6 +38,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void mangafox(View view) {
         Intent newInten = new Intent(MainActivity.this,temple_pase.class);
+        classMang mangafox = new classMang("http://mangafox.me","[class=manga_img] img[src]","[class=manga_img] ","[class=list] li");
+        mangafox.setWhere("/directory/",".html",1);
+        System.out.println("!!!!!!!!" + mangafox.getWhere());
+        mangafox.editWhere(2);
+        System.out.println("!!!!!!!!" + mangafox.getWhere());
+
         newInten.putExtra("URL","http://mangafox.me/");
         newInten.putExtra("Where","/directory");
         newInten.putExtra("Cell","[class=list] li");
---
+++ app/src/main/java/com/example/nikita/progectmangaread/classMang.java
@@ -0,0 +1,54 @@
+package com.example.nikita.progectmangaread;
+
+import android.widget.ImageView;
+
+/**
+ * Created by Nikita on 03.01.2016.
+ */
+public class classMang {
+    private String UML,imgUML,nameUML, nameCell;
+    private String whereAll,where,putch,putch2;
+
+    classMang(String UML,String imgUML, String nameUML,String nameCell){
+        this.UML = UML;
+        this.imgUML = imgUML;
+        this.nameCell = nameCell;
+        this.nameUML = nameUML;
+    }
+
+    String getUML(){ return UML; }
+
+    String getImgUML(){ return imgUML; }
+
+    String getNameUML(){ return nameUML; }
+
+    String getWhere(){
+        return whereAll;
+    }
+
+    String getNameCell(){
+        return nameCell;
+    }
+
+    //установка пути листа топа
+    void setWhere(String where,String putch,int amt){
+        this.whereAll = where + amt + putch;
+        this.where = where;
+        this.putch = putch;
+    }
+
+    void setWhere(String where,String putch,String putch2,int amt){
+        this.whereAll = where + putch + amt + putch2;
+        this.where = where;
+        this.putch = putch;
+        this.putch2 = putch2;
+    }
+
+    void editWhere(int amt){
+        if (putch2 == null) {
+            this.whereAll = where + amt + putch;
+        }else {
+            this.whereAll = where + putch + amt + putch2;
+        }
+    }
+}
