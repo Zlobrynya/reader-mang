207301acaa0a0466dff619dedf01c3fd4618a57a
Nikitin Nikita <neket95@inbox.ru>
2016-04-02 00:50:11 +0300

    slid

---
+++ app/src/main/java/com/example/nikita/progectmangaread/decode/MyImageRegionDecoder.java
@@ -0,0 +1,47 @@
+package com.example.nikita.progectmangaread.decode;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
+
+import com.davemorrissey.labs.subscaleview.decoder.ImageRegionDecoder;
+
+import rapid.decoder.BitmapDecoder;
+
+/**
+ * Created by Nikita on 01.04.2016.
+ */
+public class MyImageRegionDecoder  implements ImageRegionDecoder {
+
+    private BitmapDecoder decoder;
+
+    @Override
+    public Point init(Context context, Uri uri) throws Exception {
+        decoder = BitmapDecoder.from(context, uri);
+        decoder.useBuiltInDecoder(true);
+        return new Point(decoder.sourceWidth(), decoder.sourceHeight());
+    }
+
+    @Override
+    public synchronized Bitmap decodeRegion(Rect sRect, int sampleSize) {
+        try {
+            return decoder.reset().region(sRect).scale(sRect.width()/sampleSize, sRect.height()/sampleSize).decode();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean isReady() {
+        return decoder != null;
+    }
+
+    @Override
+    public void recycle() {
+        BitmapDecoder.destroyMemoryCache();
+        BitmapDecoder.destroyDiskCache();
+        decoder.reset();
+        decoder = null;
+    }
+}
